775b61907ff1fb9925ad65892811912df87d3cec
lucas-catto <lucascattosilva2024@gmail.com>
2025-03-06 18:51:31 -0300

    Collection Filter.

--- LaravelQueryScope/routes/web.php
+++ LaravelQueryScope/routes/web.php
@@ -1,6 +1,23 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+
+/*
+Route::get('/', function () {
+
+    $users = User::query()
+        ->select(['id', 'name'])
+        ->get()
+        ->toArray();
+    // dd($users);
+    
+    $users = User::all()
+        ->select(['id', 'name'])
+        ->toArray();
+    dd($users);
+});
+*/
 
 Route::get('/', function () {
     return view('welcome');
